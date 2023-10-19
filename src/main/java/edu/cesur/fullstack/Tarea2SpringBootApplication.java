5fa4872e0b069936fda14ab7f944fcc1001d2186
yangfan2112 <145387043+yangfan2112@users.noreply.github.com>
2023-10-19 20:04:02 +0200

    Creando proyecto

---
+++ src/main/java/edu/cesur/fullstack/Tarea2SpringBootApplication.java
@@ -0,0 +1,13 @@
+package edu.cesur.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea2SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea2SpringBootApplication.class, args);
+	}
+
+}
